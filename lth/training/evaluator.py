7f9bdddcc238b293339234d131d744f4068354c5
David Neumann <david.neumann@lecode.de>
2019-10-18 19:39:38 +0200

    During the evaluation, now, no gradients are computed, because they are not needed.

--- lth/training/evaluator.py
+++ lth/training/evaluator.py
@@ -27,29 +27,32 @@ class Evaluator:
     def evaluate(self):
         """Evaluates the model."""
 
-        # Checks if CUDA is available, in that case the evaluation is performed on the first GPU on the system, otherwise the CPU is used
-        is_cuda_available = torch.cuda.is_available()
-        device = torch.device('cuda:0' if is_cuda_available else 'cpu') # pylint: disable=no-member
+        # Since we are only evaluating the model, the gradient does not have to be computed
+        with torch.no_grad():
 
-        # Transfers the model to the selected device
-        self.model.to(device)
+            # Checks if CUDA is available, in that case the evaluation is performed on the first GPU on the system, otherwise the CPU is used
+            is_cuda_available = torch.cuda.is_available()
+            device = torch.device('cuda:0' if is_cuda_available else 'cpu') # pylint: disable=no-member
 
-        # Cycles through the whole test split of the dataset and performs the evaluation
-        self.logger.info('Evaluating the trained model...')
-        correct_predictions = 0
-        number_of_predictions = 0
-        for batch in tqdm.tqdm(self.dataset.test_split, unit='batch'):
-            inputs, labels = batch
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-            outputs = self.model(inputs)
-            _, predicted_classes = torch.max(outputs, 1) # pylint: disable=no-member
-            correctness = (predicted_classes == labels).squeeze()
-            for is_correct in correctness:
-                correct_predictions += is_correct.item()
-                number_of_predictions += 1
+            # Transfers the model to the selected device
+            self.model.to(device)
 
-        # Computes the accuracy and reports it to the user
-        accuracy = 100 * correct_predictions / number_of_predictions
-        self.logger.info('Accuracy: %f%%.', round(accuracy, 2))
-        self.logger.info('Finished evaluating the model.')
+            # Cycles through the whole test split of the dataset and performs the evaluation
+            self.logger.info('Evaluating the trained model...')
+            correct_predictions = 0
+            number_of_predictions = 0
+            for batch in tqdm.tqdm(self.dataset.test_split, unit='batch'):
+                inputs, labels = batch
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                outputs = self.model(inputs)
+                _, predicted_classes = torch.max(outputs, 1) # pylint: disable=no-member
+                correctness = (predicted_classes == labels).squeeze()
+                for is_correct in correctness:
+                    correct_predictions += is_correct.item()
+                    number_of_predictions += 1
+
+            # Computes the accuracy and reports it to the user
+            accuracy = 100 * correct_predictions / number_of_predictions
+            self.logger.info('Accuracy: %f%%.', round(accuracy, 2))
+            self.logger.info('Finished evaluating the model.')
