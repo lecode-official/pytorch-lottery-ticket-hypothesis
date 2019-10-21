Fixed a problem with the pruning. Previously the pruned weights were not frozen and could therefore change to non-zero values via training. Now, during the training, the gradients of the pruned weights is set to zero, thereby freezing their values to 0.

lth/models/lenet.py
@@ -2,9 +2,9 @@
 
 import torch
 
-from ..models.layers import LayerKind
+from .model import BaseModel, LayerKind
 
-class LeNet_300_100(torch.nn.Module): # pylint: disable=invalid-name
+class LeNet_300_100(BaseModel): # pylint: disable=invalid-name
     """Represents a much simpler LeNet variant, which has no convolutional layers."""
 
     def __init__(self, input_size=(28, 28), number_of_input_channels=1, number_of_classes=10):
@@ -76,7 +76,7 @@ class LeNet_300_100(torch.nn.Module): # pylint: disable=invalid-name
         # Returns the result
         return x
 
-class LeNet5(torch.nn.Module):
+class LeNet5(BaseModel):
     """
     Represents the classical convolutional neural network architecture LeNet5 introduced by Yann LeCun et al. in their paper "Gradient-Based Learning
     Applied to Document Recognition.", where it was used for character recognition.

lth/pruning/magnitude_pruning.py
@@ -5,8 +5,6 @@ import logging
 import tqdm
 import torch
 
-from ..models.layers import Layer
-
 class LayerWiseMagnitudePruner:
     """
     Represents a pruning method that prunes away a certain layer-specific percentage of the weights of a neural network model that have the lowest
@@ -27,22 +25,22 @@ class LayerWiseMagnitudePruner:
         self.model = model
         self.logger = logging.getLogger('lth.pruning.magnitude_pruning.LayerWiseMagnitudePruner')
 
+    def prune(self):
+        """Generates the pruning masks for the model and prunes it."""
+
+        self.create_pruning_masks()
+        self.apply_pruning_masks()
+
     def create_pruning_masks(self):
         """
-        Generates the pruning masks for all layers of the model.
-
-        Returns
-        -------
-            dict
-                Returns a dictionary containing a pruning mask for each layer in the model. The pruning is not performed in-place on the layers of the
-                model itself but a pruning mask is created for each layer (of the same shape as the layer), which is has 0 values for all weights in
-                the layer that were pruned and values of 1 for all weights that were not pruned.
+        Generates the pruning masks for all layers of the model. The pruning is not performed in-place on the layers of the model itself but a pruning
+        mask is created for each layer (of the same shape as the layer), which has 0 values for all weights in the layer that were pruned and values
+        of 1 for all weights that were not pruned.
         """
 
         # Creates the pruning masks for each layer of the model
-        masks = {}
         self.logger.info('Generating pruning mask for model %s...', self.model.name)
-        for layer in tqdm.tqdm(Layer.get_layers_from_model(self.model), unit='layer'):
+        for layer in tqdm.tqdm(self.model.get_layers(), unit='layer'):
 
             # Determines the pruning rate of the layer, if the pruning rate is 0.0, then no pruning is performed on the layer
             layer_pruning_rate = self.get_layer_pruning_rate(layer)
@@ -64,19 +62,19 @@ class LayerWiseMagnitudePruner:
             # by magnitude and then take the smallest n%, the same weights that are already zero would be pruned in any consecutive pruning,
             # therefore, the number of zeros in the layer are added to the number of pruned weights, otherwise no further pruning would occur
             number_of_zero_weights = weights.numel() - weights.nonzero().size(0)
-            number_of_pruned_weights = int(layer_pruning_rate * len(sorted_indices)) + number_of_zero_weights
+            number_of_pruned_weights = int(layer_pruning_rate * (len(sorted_indices) - number_of_zero_weights)) + number_of_zero_weights
 
             # Creates the pruning mask which is 1 for all weights that are not pruned and
             pruning_mask = torch.zeros_like(weights, dtype=torch.uint8)
             pruning_mask[sorted_indices[:number_of_pruned_weights]] = 0
             pruning_mask[sorted_indices[number_of_pruned_weights:]] = 1
 
-            # Reshapes the pruning mask to the original shape of the weights of the layer
-            masks[layer.name] = pruning_mask.reshape(layer.weights.shape)
+            # Reshapes the pruning mask to the original shape of the weights of the layer and stores it in the model
+            pruning_mask = pruning_mask.reshape(layer.weights.shape)
+            self.model.update_pruning_mask(layer.name, pruning_mask)
 
-        # Returns the pruning masks for all layers
+        # Logs out a success message
         self.logger.info('Finished generating pruning mask for model %s.', self.model.name)
-        return masks
 
     def get_layer_pruning_rate(self, layer):
         """
@@ -102,21 +100,15 @@ class LayerWiseMagnitudePruner:
             return self.model.pruning_rates[layer.king]
         return 0.0
 
-    def apply_pruning_masks(self, pruning_masks):
-        """
-        Applies the pruning masks generated using create_pruning_masks. This is effectively the actual pruning.
-
-        Parameters
-        ----------
-            pruning_masks: dict
-                A dictionary where the keys are the names of the layers and the values are the pruning masks for the layer.
-        """
+    def apply_pruning_masks(self):
+        """Applies the pruning masks generated using create_pruning_masks. This is effectively the actual pruning."""
 
         # Applies the pruning masks for all layers
         total_number_of_weights = 0
         number_of_pruned_weights = 0
         number_of_zero_weights = 0
         self.logger.info('Applying pruning masks to the layers of the model...')
+        pruning_masks = self.model.get_pruning_masks()
         for layer_name in tqdm.tqdm(pruning_masks, unit='layer'):
             layer_weights = self.model.state_dict()['{0}.weight'.format(layer_name)]
             total_number_of_weights += layer_weights.numel()